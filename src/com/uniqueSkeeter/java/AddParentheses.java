a5096abda4f57c108dc5d335a973d7cec3cefad5
uniqueSkeeter <wenrui199231@gmail.com>
2015-08-04 21:46:04 +0800

    ss

---
+++ src/com/uniqueSkeeter/java/AddParentheses.java
@@ -0,0 +1,21 @@
+package com.uniqueSkeeter.java;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class AddParentheses {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	public List<Integer> diffWaysToCompute(String input) {
+	        List<Integer> res = null;
+	        if(input == null)
+	        	return res; 
+	        res = new ArrayList<Integer>();
+	        
+	        return res;
+	}
+}
