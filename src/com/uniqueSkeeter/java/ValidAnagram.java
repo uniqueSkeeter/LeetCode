ec5f9e075ddaef952f37c142b9cfcf7d9752db56
uniqueSkeeter <wenrui199231@gmail.com>
2015-08-04 20:09:23 +0800

    ss

--- src/com/uniqueSkeeter/java/ValidAnagram.java
+++ src/com/uniqueSkeeter/java/ValidAnagram.java
@@ -1,5 +1,15 @@
 package com.uniqueSkeeter.java;
 //判断两个单词是否为易位构词
+/*
+ Given two strings s and t, write a function to determine if t is an anagram of s.
+
+For example,
+s = "anagram", t = "nagaram", return true.
+s = "rat", t = "car", return false.
+
+Note:
+You may assume the string contains only lowercase alphabets.
+ */
 public class ValidAnagram {
 
 	public static void main(String[] args) {
