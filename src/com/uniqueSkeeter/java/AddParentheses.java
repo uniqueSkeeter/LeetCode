1e22ca46df616a2398c566261cb92271ed66e465
uniqueSkeeter <wenrui199231@gmail.com>
2015-08-05 20:46:20 +0800

    ss

--- src/com/uniqueSkeeter/java/AddParentheses.java
+++ src/com/uniqueSkeeter/java/AddParentheses.java
@@ -3,7 +3,18 @@ package com.uniqueSkeeter.java;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Stack;
+/*
+ Given a string of numbers and operators, 
+ return all possible results from computing all the different possible ways to group numbers and operators. 
+ The valid operators are +, - and *.
 
+Example 1
+Input: "2-1-1".
+
+((2-1)-1) = 0
+(2-(1-1)) = 2
+Output: [0, 2]
+ */
 public class AddParentheses {
 
 	public static void main(String[] args) {
