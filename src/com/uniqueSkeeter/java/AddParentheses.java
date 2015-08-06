9358f6eff5df6557ff2a305d4492a05b75c001d0
uniqueSkeeter <wenrui199231@gmail.com>
2015-08-06 20:38:18 +0800

    df

--- src/com/uniqueSkeeter/java/AddParentheses.java
+++ src/com/uniqueSkeeter/java/AddParentheses.java
@@ -22,11 +22,48 @@ public class AddParentheses {
 
 	}
 	public List<Integer> diffWaysToCompute(String input) {
-	        List<Integer> res = null;
-	        if(input == null)
-	        	return res; 
-	        res = new ArrayList<Integer>();
-	        
-	        return res;
+		int val = 0, idx = 0;
+	    while (idx < input.length() && isDigit(input.charAt(idx))) {
+	    	val *= 10;
+	        val += input.charAt(idx) - '0';
+	        idx++;
+	    }
+	    if (idx == input.length()){
+	    	List<Integer> temp = new ArrayList<Integer>();
+	    	temp.add(val);
+	    	return temp;
+	    }
+	    List<Integer> res = null;
+	    List<Integer> left, right;
+	    for (int i = 0; i < input.length(); ++i) {
+	    	if (!isDigit(input.charAt(i))) {
+	    		left = diffWaysToCompute(input.substring(0,i));
+	    		right = diffWaysToCompute(input.substring(i + 1, input.length() -1 - i));
+	            for (int j = 0; j < left.size(); ++j) {
+	            	for (int k = 0; k < right.size(); ++k) {
+	            		res.add(computeUnit(left.get(j), right.get(k), input.charAt(i)));
+	                }
+	            }
+	        }
+	    }
+	    return res;
 	}
+	public int computeUnit(int a,int b, char op){
+		switch(op){
+		case'+':return a+b;
+		case'-':return a-b;
+		case'*':return a*b;
+		default:{
+			System.out.println("error operator !");
+			return -1;
+			}
+		}
+	}
+	public boolean isDigit(char ch){
+		if(ch >= '0' && ch <= '9')
+			return true;
+		else
+			return false;
+	}
+	
 }
